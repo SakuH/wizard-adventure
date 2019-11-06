602915141f0671e2f4d91984cc275f6e25e45fa9
Saku Huuha <rikottuneljasseina@gmail.com>
2019-11-06 12:07:10 +0000

    "boss" spawns during level generation

--- Assets/Scripts/Level Generation/RoomTemplates.cs
+++ Assets/Scripts/Level Generation/RoomTemplates.cs
@@ -10,4 +10,24 @@ public class RoomTemplates : MonoBehaviour
     public GameObject[] rightRooms;
 
     public GameObject closedRoom;
+
+    public List<GameObject> rooms;
+
+    public float waitTime;
+    private bool spawnedBoss;
+    public GameObject boss;
+
+    private void Update()
+    {
+        if(waitTime <= 0 && spawnedBoss == false)
+        {
+            
+            Instantiate(boss, rooms[rooms.Count - 1].transform.position + new Vector3(0,2), Quaternion.identity);
+            spawnedBoss = true;
+        }
+        else
+        {
+            waitTime -= Time.deltaTime;
+        }
+    }
 }
