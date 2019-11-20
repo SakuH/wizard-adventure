8d02dd4c26be45c1e915bc6eb0045ac3fdc0e6ec
Matias Röyttä <matiass6699@gmail.com>
2019-11-20 13:44:56 +0000

    trap

---
+++ Assets/Scripts/Enemies/Trap.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Trap : MonoBehaviour
+{
+
+    public GameObject player;
+
+    public int damage;
+
+    private float nextFire;
+
+    private bool canTakeDamage;
+
+    public float timeBetweenShots;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        canTakeDamage = false;
+        player = GameObject.FindGameObjectWithTag("Player");
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Time.time > nextFire && canTakeDamage)
+        {
+
+        nextFire = Time.time + timeBetweenShots;
+              
+        player.GetComponent<PlayerHealth>().takeDamage(damage);
+
+        }
+        
+        
+    }
+
+    private void OnTriggerEnter(Collider other) {
+        if(other.gameObject.CompareTag("Player"))
+        {
+            canTakeDamage = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other) {
+        if(other.gameObject.CompareTag("Player"))
+        {
+            canTakeDamage = false;
+        }
+    }
+}
