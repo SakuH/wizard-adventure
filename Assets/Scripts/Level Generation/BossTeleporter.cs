27a5b5be303f1259c9ffc20d4207311ffda78bea
Saku Huuha <rikottuneljasseina@gmail.com>
2019-11-27 13:58:15 +0000

    cleanup

--- Assets/Scripts/Level Generation/BossTeleporter.cs
+++ Assets/Scripts/Level Generation/BossTeleporter.cs
@@ -18,7 +18,7 @@ public class BossTeleporter : MonoBehaviour
         {
             Instantiate(enemyBoss, new Vector3(0, 102.0f, 20), Quaternion.identity);
             other.GetComponent<Transform>().SetPositionAndRotation(new Vector3(0, 102.0f, 0), Quaternion.identity);
-            other.GetComponent<PlayerMovement>().SavePlayerData(2);
+            //other.GetComponent<PlayerMovement>().SavePlayerData(2);
         }
     }
 }
