13cf870bc40754ff09c1de52111463dbb2555983
Niko Hämäläinen <hollow087@hotmail.com>
2019-12-02 12:06:00 +0000

    intro

---
+++ Assets/Scripts/GameManagement/IntroSceneManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class IntroSceneManager : MonoBehaviour
+{
+    public GameObject teleportPrefab;
+    public int nextScene;
+    public GameObject teleportObject;
+    void Start()
+    {
+        
+    }
+
+   
+    void Update()
+    {
+        
+    }
+    public void loadLevelOne()
+    {
+        SceneManager.LoadScene(nextScene);
+    }
+    public void instantiateTeleportAnimation()
+    {
+        Vector3 position = transform.position;
+        
+        Instantiate(teleportPrefab, teleportObject.transform.position, transform.rotation);
+        
+    }
+}
