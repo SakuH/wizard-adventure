e27a947f4681e8419cb889ae1394aeee1c6c3710
Matias Röyttä <matiass6699@gmail.com>
2019-12-03 12:24:49 +0000

    deleted assets

---
+++ Assets/Scripts/TutorialLevelChange.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TutorialLevelChange : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public int levelNumber;
+
+   
+
+   private void OnTriggerEnter(Collider other) {
+       SceneManager.LoadScene(levelNumber);
+   }
+}
