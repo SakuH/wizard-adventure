af9aa668b4cbe65c0f4cbac5a703ad1cf24db0db
Saku Huuha <rikottuneljasseina@gmail.com>
2019-12-09 16:49:23 +0000

    boss room has more enemies

--- Assets/Scripts/Level Generation/ObjectGenerator.cs
+++ Assets/Scripts/Level Generation/ObjectGenerator.cs
@@ -7,30 +7,43 @@ public class ObjectGenerator : MonoBehaviour
     public GameObject parentRoom;
     public GameObject[] objects;
 
+    public bool isPartOfRoom = true;
+
     private RoomInternalBehaviour roomBehaviour;
 
     void Start()
     {
-        roomBehaviour = GetComponentInParent<RoomInternalBehaviour>();
+        if (isPartOfRoom)
+        {
+            roomBehaviour = GetComponentInParent<RoomInternalBehaviour>();
+        }
     }
 
     public void SpawnObject()
     {
         int rand = Random.Range(0, objects.Length);
-        if (objects[rand].CompareTag("Enemy"))
+        
+        if (isPartOfRoom)
         {
-            roomBehaviour.AddEnemyToRoom(Instantiate(objects[rand], transform.position, Quaternion.identity));
-        }
-        else if (objects[rand].CompareTag("Door"))
-        {
-            roomBehaviour.AddDoorToRoom(Instantiate(objects[rand], transform.position, objects[rand].transform.rotation));
-        }
-        else if(objects[rand].CompareTag("Weapon"))
-        {   
-            GameObject random = objects[rand];
-           // random.GetComponent<GunController>().spawnedFromSpawner = true;
-            Instantiate(random, transform.position, Quaternion.identity);
-           //objects[rand].GetComponent<GunController>().playerChildCount = objects[rand].GetComponent<GunController>().playerChildCount - 1;
+            if (objects[rand].CompareTag("Enemy"))
+            {
+                roomBehaviour.AddEnemyToRoom(Instantiate(objects[rand], transform.position, Quaternion.identity));
+            }
+            else if (objects[rand].CompareTag("Door"))
+            {
+                roomBehaviour.AddDoorToRoom(Instantiate(objects[rand], transform.position, objects[rand].transform.rotation));
+            }
+            else if(objects[rand].CompareTag("Weapon"))
+            {   
+                GameObject random = objects[rand];
+               // random.GetComponent<GunController>().spawnedFromSpawner = true;
+                Instantiate(random, transform.position, Quaternion.identity);
+               //objects[rand].GetComponent<GunController>().playerChildCount = objects[rand].GetComponent<GunController>().playerChildCount - 1;
+            }
+            else
+            {
+                Instantiate(objects[rand], transform.position, Quaternion.identity);
+            }
         }
         else
         {
