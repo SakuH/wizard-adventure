fb5a60ff326834806d18cf6c02004fb3b873e770
Niko Hämäläinen <hollow087@hotmail.com>
2019-12-10 14:15:05 +0000

    endingscene

---
+++ Assets/Scripts/endingSceneScripts/FadeToBlackEnding.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class FadeToBlackEnding : MonoBehaviour
+{
+    public bool fadeToBlack= false;
+    public Image backgroundColor;
+    public float fadeToBlackColor;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (fadeToBlack)
+        {
+            fadeToBlackCanvas();
+        }
+    }
+    public void fadeToBlackCanvas()
+    {
+        var tempColor = backgroundColor.color;
+
+        if (fadeToBlackColor < 1)
+        {
+            fadeToBlackColor += Time.deltaTime;
+        }
+
+        tempColor.a = fadeToBlackColor;
+        backgroundColor.color = tempColor;
+
+    }
+    public void callFadeToBlack()
+    {
+        fadeToBlack = true;
+    }
+     
+    }
