61409c831e7d9e56f0c2fe55209bf0be81182f63
Niko Hämäläinen <hollow087@hotmail.com>
2019-12-11 13:01:09 +0000

    intro and ending scene some sounds

---
+++ Assets/Scripts/IntroSceneSounds/IntroSounds.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IntroSounds : MonoBehaviour
+{
+    public AudioSource introSoundMusic;
+    public AudioSource introPlayerSfx;
+    public AudioSource introPlayerSfx2;
+    public AudioClip doorClosed;
+     
+   public void changeSfx()
+    {
+        introPlayerSfx.clip = doorClosed;
+    }
+}
